380007e7ef260a237ce5414d34d38ee004a21a30
Fatih AYTAÇ <fatih.aytac@gmail.com>
2018-10-12 16:37:32 +0300

    Revert "Updated mysql username."
    This reverts commit 7d37980c18ea9441d5c91589916a7bacafe6d188.

--- puphpet/files/bundle.sh
+++ puphpet/files/bundle.sh
@@ -3,7 +3,7 @@ cd /var/oopla && bundle install
 
 bundle exec rake db:create
 
-mysql -u oopla -p123123 oopla_development < /vagrant/puphpet/files/sql/generatedb_oopla.sql
-mysql -u oopla -p123123 oopla_development < /vagrant/puphpet/files/sql/data_collections.sql
-mysql -u oopla -p123123 oopla_development < /vagrant/puphpet/files/sql/data_games.sql
-mysql -u oopla -p123123 oopla_development < /vagrant/puphpet/files/sql/data_collection_items.sql
+mysql -u root -p123123 oopla_development < /vagrant/puphpet/files/sql/generatedb_oopla.sql
+mysql -u root -p123123 oopla_development < /vagrant/puphpet/files/sql/data_collections.sql
+mysql -u root -p123123 oopla_development < /vagrant/puphpet/files/sql/data_games.sql
+mysql -u root -p123123 oopla_development < /vagrant/puphpet/files/sql/data_collection_items.sql
